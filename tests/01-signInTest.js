5ca5fc57327003036f2716e5988bdd4734228d40
VasylQA <vasbklan@gmail.com>
2018-08-16 21:38:26 +0300

    tette

--- tests/01-signInTest.js
+++ tests/01-signInTest.js
@@ -7,7 +7,7 @@ module.exports = {
     browser.end();
   },
 
-   'Sign in with invalid password': function (browser) {
+  'Sign in with invalid password': function (browser) {
     browser
       .waitForElementVisible('.account-cart-wrapper a[href*="account"]', browser.globals.smallWait)
       .click('.account-cart-wrapper a[href*="account"]')
@@ -51,9 +51,28 @@ module.exports = {
       .waitForElementVisible('.page-title h1', browser.globals.smallWait);
 
     browser.assert.containsText('.page-title h1', 'MY DASHBOARD');
-
-
     //ToDo: write test for successful sign in
     //pass for the account above 'QA92837465'
+  },
+
+   'Go to "Clothes" Category': function(browser){
+ browser
+      .useXpath()
+      .waitForElementVisible('//a[text()="clothes"]', browser.globals.smallWait)
+      .click('//a[text()="clothes"]')
+      .useCss()
+      .waitForElementVisible('.page-title h1', browser.globals.smallWait);
+
+    browser.assert.containsText('.page-title h1', 'CLOTHES');
+  },
+  'Add to Cart product': function(browser){
+ browser
+      .useXpath()
+      .waitForElementVisible('//a[contains(text(),"slippers_simple_product")]/parent::h2/following-sibling::div/following-sibling::div/button', browser.globals.smallWait)
+      .click('//a[contains(text(),"slippers_simple_product")]/parent::h2/following-sibling::div/following-sibling::div/button')
+      .useCss()
+      .waitForElementVisible('.success-msg', browser.globals.smallWait);
+
+    browser.assert.containsText('.success-msg', 'was added to your shopping cart.');
   }
 };
